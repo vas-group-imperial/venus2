fixed batch normalisation bug

venus/bounds/equation.py
@@ -181,9 +181,6 @@ class Equation():
         elif isinstance(node, MatMul):
             tr_eq = self._transpose_linear(node, in_flag, out_flag)
 
-        elif isinstance(node, BatchNormalization):
-            tr_eq = self._transpose_batch_normalization(node, in_flag, out_flag)
-
         elif isinstance(node, Slice):
             tr_eq = self._transpose_slice(node)
 
@@ -237,26 +234,6 @@ class Equation():
 
         return Equation(matrix, self.const, self.config)
         
-    def _transpose_batch_normalization(self, node: Node, in_flag: torch.tensor, out_flag: torch.tensor):
-        in_ch_sz = node.in_ch_sz()
-
-        scale = torch.tile(node.scale, (in_ch_sz, 1)).T.flatten()
-        bias = torch.tile(node.bias, (in_ch_sz, 1)).T.flatten()
-        input_mean = torch.tile(node.input_mean, (in_ch_sz, 1)).T.flatten()
-        var = torch.sqrt(node.input_var + node.epsilon)
-        var = torch.tile(var, (in_ch_sz, 1)).T.flatten()
-
-        if in_flag is None:
-            matrix = (self.matrix * scale) / var
-        else:
-            prop_flag = in_flag.flatten()
-            matrix = (self.matrix[:, prop_flag] * scale[prop_flag]) / var[prop_flag]
-
-        batch_const = - input_mean / var * scale + bias
-        const = self.matrix @ batch_const + self.const
-
-        return Equation(matrix, const, self.config)
-
     def _transpose_slice(self, node: Node):
         matrix = torch.zeros(
             (self.matrix.size, node.input_size),
@@ -296,6 +273,13 @@ class Equation():
             return [
                 self.interval_relu_transpose(
                     node, bound, in_flag, out_flag, slopes
+                )
+            ]
+
+        elif isinstance(node, BatchNormalization):
+            return [
+                self._interval_batch_normalization_transpose(
+                    node, bound, in_flag, out_flag
                 )
             ]
 
@@ -419,6 +403,37 @@ class Equation():
             upper_const[idxs]  -= upper_slope[idxs] *  lower[idxs]
 
         return (lower_slope, upper_slope), (lower_const, upper_const)
+
+
+    def _interval_batch_normalization_transpose(
+        self,
+        node: Node,
+        in_flag: torch.tensor,
+        out_flag: torch.tensor
+    ):
+        in_ch_sz = node.in_ch_sz()
+
+        scale = torch.tile(node.scale, (in_ch_sz, 1)).T.flatten()
+        bias = torch.tile(node.bias, (in_ch_sz, 1)).T.flatten()
+        input_mean = torch.tile(node.input_mean, (in_ch_sz, 1)).T.flatten()
+        var = torch.sqrt(node.input_var + node.epsilon)
+        var = torch.tile(var, (in_ch_sz, 1)).T.flatten()
+
+        scale_var = scale / var
+        idxs = scale_var < 0
+        scale_var[idxs] = -scale_var[idxs]
+
+        if in_flag is None:
+            matrix = self.matrix * scale_var
+        else:
+            prop_flag = in_flag.flatten()
+            matrix = self.matrix[:, prop_flag] * scale_var[prop_flag]
+
+        batch_const = - input_mean / var * scale + bias
+        const = self.matrix @ batch_const + self.const
+
+        return Equation(matrix, const, self.config)
+
 
     def __get_relu_relaxation(self, node: Node) -> tuple:
         lower_slope = torch.ones(

venus/bounds/sip.py
@@ -85,11 +85,15 @@ class SIP:
                 if j.bounds.size() > 0 and saw_symb_eq_node is not True:
                     continue
 
-                delta = delta_flags[j.to_node[0]] if delta_flags is not None and j.has_relu_activation() else None
+                if delta_flags is not None and j.has_relu_activation() is True:
+                    delta = delta_flags[j.to_node[0]] 
+                else:
+                    delta = None
 
                 self.set_ia_bounds(j, slopes=slopes, delta_flags=delta)
  
-                if self.is_symb_eq_eligible(j) is not True:
+                if self.is_symb_eq_eligible(j) is not True or 
+                self.config.SIP.SYMBOLIC_BOUNDS is not True:
                     continue
 
                 if saw_symb_eq_node is not True:
@@ -100,12 +104,13 @@ class SIP:
 
                 # print(j.id, torch.mean(j.bounds.lower), torch.mean(j.bounds.upper))
 
-        # self.set_ia_bounds(self.prob.nn.tail, slopes=slopes)
-        self.prob.nn.tail.bounds = Bounds(
-            torch.ones(self.prob.nn.tail.output_shape) * -math.inf,
-            torch.ones(self.prob.nn.tail.output_shape) * math.inf,
-        )
-        self.set_symb_concr_bounds(self.prob.nn.tail, slopes)
+        self.set_ia_bounds(self.prob.nn.tail, slopes=slopes)
+        # self.prob.nn.tail.bounds = Bounds(
+            # torch.ones(self.prob.nn.tail.output_shape) * -math.inf,
+            # torch.ones(self.prob.nn.tail.output_shape) * math.inf,
+        # )
+        if self.config.SIP.SYMBOLIC_BOUNDS is True:
+            self.set_symb_concr_bounds(self.prob.nn.tail, slopes)
 
         # print(
             # self.prob.nn.tail.id,
@@ -139,7 +144,8 @@ class SIP:
         inp = node.from_node[0].bounds
 
         if type(node) in [Relu, BatchNormalization, MaxPool, Slice, Unsqueeze]:
-            lower, upper = node.forward(inp.lower), node.forward(inp.upper)
+            f_l, f_u = node.forward(inp.lower), node.forward(inp.upper)
+            lower, upper = torch.min(f_l, f_u), torch.max(f_l, f_u)
 
         elif isinstance(node, Flatten):
             lower, upper = inp.lower, inp.upper

venus/network/neural_network.py
@@ -284,7 +284,10 @@ class NeuralNetwork:
         for i in range(self.tail.depth + 1):
             nodes = self.get_node_by_depth(i)
             for j in nodes:
-                j.forward_numpy(save_output=True)     
+                j.forward(save_output=True)     
+                print(j, torch.mean(j.output))
+                if isinstance(j, Conv):
+                    print(j.kernels.shape, j.pads, j.strides)
 
         output = self.tail.output
         self.clean_outputs()

venus/solver/milp_encoder.py
@@ -62,14 +62,13 @@ class MILPEncoder:
             env.start()
 
             gmodel = Model(env=env)
+
             self.add_node_vars(gmodel, linear_approx)
 
             gmodel.update()
 
             self.add_node_constrs(gmodel, linear_approx)
-
             self.add_output_constrs(self.prob.nn.tail, gmodel)
-
             deps_cond = linear_approx is not True and \
                 (
                     self.config.SOLVER.INTRA_DEP_CONSTRS is True or \
@@ -115,10 +114,10 @@ class MILPEncoder:
                         self.add_relu_delta_vars(j, gmodel)
 
                 elif type(j) in [Flatten, Slice, Unsqueeze]:
-                    j.out_vars = j.forward_numpy(j.from_node[0].out_vars)
+                    j.out_vars = j.forward(j.from_node[0].out_vars)
 
                 elif isinstance(j, Concat):
-                    j.out_vars = j.forward_numpy([k.out_vars for k in j.from_node])
+                    j.out_vars = j.forward([k.out_vars for k in j.from_node])
 
                 elif type(j) in [Gemm, MatMul, Conv, ConvTranspose, Sub, BatchNormalization, MaxPool]:
                     self.add_output_vars(j, gmodel)
@@ -137,8 +136,6 @@ class MILPEncoder:
             gmodel:
                 The gurobi model
         """ 
-        node.out_vars = np.empty(shape=node.output_shape, dtype=Var)
-
         if node.bounds.size() > 0:
             node.out_vars = np.array(
                 gmodel.addVars(
@@ -235,12 +232,12 @@ class MILPEncoder:
             raise TypeError(f"Cannot compute sub onstraints for {type(node)} nodes.")
 
         if type(node) in ['Sub', 'Add'] and node.const is not None:
-            output = node.forward_numpy(
+            output = node.forward(
                 node.from_node[0].out_vars, node.from_node[1].out_vars
             )
  
         else:
-            output = node.forward_numpy(node.from_node[0].out_vars)
+            output = node.forward(node.from_node[0].out_vars)
 
         for i in node.get_outputs():
             gmodel.addConstr(node.out_vars[i] == output[i])
@@ -259,7 +256,7 @@ class MILPEncoder:
         """
         assert(isinstance(node, Relu)), "Cannot compute relu constraints for non-relu nodes."
          
-        inp = node.from_node[0].forward_numpy(node.from_node[0].from_node[0].out_vars)
+        inp = node.from_node[0].forward(node.from_node[0].from_node[0].out_vars)
         out, delta = node.out_vars, node.delta_vars
         l, u = node.from_node[0].bounds.lower, node.from_node[0].bounds.upper
 
@@ -329,15 +326,13 @@ class MILPEncoder:
             padded_inp, node.kernel_shape, node.strides
         )
 
-        idxs = np.array(node.get_outputs).reshape(
-            node.output_shape_no_batch
-        ).tranpose(2, 0, 1).reshape(np.prod(kernel_shape), node.in_ch())
+        idxs = np.arange(node.output_size).reshape(
+            node.output_shape_no_batch()
+        ).transpose(1, 2, 0).reshape(-1, node.in_ch())
 
-        for i, j in itertools.product(
-                range(np.prod(node.kernel_shape), range(node.in_ch()))
-        ):
+        for i in itertools.product(*[range(j) for j in idxs.shape]):
             gmodel.addConstr(
-                node.out_vars[idxs[i, j]] == max_(im2col[:, i, j].tolist())
+                np.take(node.out_vars, idxs[i]) == max_(im2col[:, i[0], i[1]].tolist())
             )
 
         # for i in node.get_outputs():